7f0e63e543f640b2d744182c7d92da1347039466
Midhun Mohanan P <midhuntechxora@gmail.com>
2019-08-22 12:18:08 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TeacherDetailsComponent } from './teacher-details/teacher-details.component';
+import { TeacherDetailsListComponent } from './teacher-details-list/teacher-details-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeacherDetailsComponent,
+    TeacherDetailsListComponent
   ],
   imports: [
     BrowserModule
